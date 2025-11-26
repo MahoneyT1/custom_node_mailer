1c68f627c5da9794a07d0928bf4c2b485acf7662
MahoneyT1 <ifeanyitech15@gmail.com>
2025-11-26 22:52:22 +0100

    complete api

--- api/send-email.ts
+++ api/send-email.ts
@@ -12,6 +12,24 @@ const transporter = nodemailer.createTransport({
 });
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
+    const allowedOrigins = [
+    "http://localhost:5173",
+    "http://leaves-admin.com",
+    "https://leaves-admin.com"
+  ];
+
+const origin = req.headers.origin ?? "";
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({ success: false, error: "Method not allowed" });
   }
